19641f31b17a280d42f29d44995966e45ea1a5a6
DmytroMykolaichuk <dmitriymikolaychuk@gmail.com>
2023-02-04 10:05:35 +0200

    cosmetic

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,20 +3,20 @@ import Notiflix from "notiflix";
 const formEl = document.querySelector('form');
 const delayBtnEl = document.querySelector('[name="delay"]');
 const delayStepBtnEl = document.querySelector('[name="step"]');
-const amountBtnEl = document.querySelector('[name="amount"]')
+const amountBtnEl = document.querySelector('[name="amount"]');
 
 formEl.addEventListener('submit', e => {
-  e.preventDefault()
+  e.preventDefault();
   
-  let position = 1
+  let position = 1;
   let delay = +delayBtnEl.value;
 
   for(let i = 0; i < +amountBtnEl.value; i += 1){
     createPromise(position, delay)
-      .then(({ position, delay}) => Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
-      .catch(({ position, delay}) => Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
-    position += 1
-    delay += +delayStepBtnEl.value
+      .then(({ position, delay }) => Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+      .catch(({ position, delay }) => Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+    position += 1;
+    delay += +delayStepBtnEl.value;
   }
   // e.target.reset()
 })
@@ -30,14 +30,14 @@ function createPromise(position, delay) {
         const value = {
           position,
           delay,
-        }
+        };
         resolve(value);
       } else {
         const error = {
           position,
           delay,
-      }
-    reject(error);
+        };
+        reject(error);
       }
   },delay)
   })
